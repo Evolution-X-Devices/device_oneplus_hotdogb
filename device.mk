ed75e9d973602344ed0426216c12d67764dc4378
AnierinB <anierin@evolution-x.org>
2022-10-22 22:32:20 +0000

    hotdogb: Configure OPlusExtras device specific features
    Co-authored-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_PACKAGES += \
     ApertureResTarget \
     EvolutionSettingsResTarget \
     EvolutionSystemUIResTarget \
+    OPlusExtrasResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
